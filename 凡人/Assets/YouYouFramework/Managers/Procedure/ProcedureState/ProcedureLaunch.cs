e7ceb6cf8c24442967309b9ce9105a0523af9da2
wangyibo <89531734@qq.com>
2019-12-17 00:13:07 +0800

    12.17

---
+++ 凡人/Assets/YouYouFramework/Managers/Procedure/ProcedureState/ProcedureLaunch.cs
@@ -0,0 +1,40 @@
+//===================================================
+//作    者：边涯  http://www.u3dol.com
+//创建时间：
+//备    注：
+//===================================================
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YouYou
+{
+    /// <summary>
+    /// 启动流程
+    /// </summary>
+    public class ProcedureLaunch : ProcedureBase
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            GameEntry.Log(LogCategory.Procedure, "OnEnter ProcedureLaunch");
+        }   
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnLeave()
+        {
+            base.OnLeave();
+            GameEntry.Log(LogCategory.Procedure, "OnLeave ProcedureLaunch");
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+    }
+}
